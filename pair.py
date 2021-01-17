8d61168f169e59f802eddb5d64c1470a9abb08f4
Max Karpawich <maxkarpawich@gmail.com>
2021-01-16 20:28:16 -0500

    feat: add comments

--- pair.py
+++ pair.py
@@ -20,22 +20,31 @@ projects = [
 ]
 
 matrix = []
+# open the CSV file
 with open(csv_filename, newline=csv_newline) as file:
     csv_rows = list(csv.DictReader(file))
     proj_member_ratio = int(len(csv_rows) / len(projects))
+
+    # enumerate over each row of the CSV
     for i, csv_row in enumerate(csv_rows):
         semester_count = int(csv_row['Semesters']) + 1
         matrix_row = [0] * len(csv_rows)
+
+        # construct a corresponding row in the cost matrix
         for j in range(len(projects)):
             proj_i = projects.index(csv_row[str(j + 1)])
+            # duplicate project nodes as needed
             for k in range(proj_member_ratio):
                 matrix_row[proj_i * proj_member_ratio + k] = (j + 1) * semester_count
         matrix.append(matrix_row)
 
+    # construct a numpy array from the cost matrix
     matrix = np.array(matrix)
 
+    # run the cost matrix through the linear sum assignment function
     _, col_indices = linear_sum_assignment(matrix)
 
+    # combine the duplicated project nodes together & construct teams
     teams = [[] for i in range(len(projects))]
     for i, col_index in enumerate(col_indices):
         proj_index = int(int(col_index) / proj_member_ratio)
